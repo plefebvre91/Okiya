d3b50e3ea3271b6007e868bacf3a59fc018f9202
plefebvre <plefebvre@enseirb-matmeca.fr>
2015-03-29 13:29:56 +0200

    Début définition classe Tile/Board

---
+++ src/Definitions.hpp
@@ -0,0 +1,13 @@
+//Constraint types
+#define NO_CONSTRAINT  0x0000
+#define OKIYA_FLOWER   0x0001
+#define OKIYA_BIRD     0x0002
+#define OKIYA_SUN      0x0004
+#define OKIYA_SKY      0x0008
+#define OKIYA_TREE     0x0010
+#define OKIYA_LEAF     0x0020
+#define OKIYA_CACTUS   0x0040
+#define OKIYA_REDTHING 0x0080
+
+//Global information
+#define OKIYA_NB_TILES 16
